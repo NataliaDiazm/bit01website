39c66b90479275bf2ef7cd6c8cdd43db18e477eb
Natalia <natalia.contactod@gmail.com>
2023-05-24 18:15:31 -0500

    proyecto website

---
+++ assets/components/contact/contact.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById("modal");
+const cerrarBtn = document.getElementsByClassName("closed")[0];
+
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Para obtener los valores del formulario
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+  modal.style.display = "block";
+
+  console.log("Nombre:", nombre);
+  console.log(email);
+  console.log(mensaje);
+});
+
+cerrarBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+  location.reload();
+  
+});
